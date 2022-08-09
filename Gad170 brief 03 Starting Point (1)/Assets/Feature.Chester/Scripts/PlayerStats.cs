20a7fde3fd3ebdd7be45b76c9b72b56e71c7f397
ChesterRocela <107157644+ChesterRocela@users.noreply.github.com>
2022-08-10 03:37:22 +1000

    commit 10/08

---
+++ Gad170 brief 03 Starting Point (1)/Assets/Feature.Chester/Scripts/PlayerStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChesterRocela
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        // Health script that can be called out from PowerUp script
+        public float health = 100f;
+    }
+}
--- Gad170 brief 03 Starting Point (1)/Assets/Feature.Chester/Scripts/Powerup.cs
+++ Gad170 brief 03 Starting Point (1)/Assets/Feature.Chester/Scripts/Powerup.cs
@@ -1,26 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ChesterRocela
 {
     public class Powerup : MonoBehaviour
     {
         public float multiplier = 2f;
+        public float duration = 4f;
+        public float normalSizetank = 5f;
+
         void OnTriggerEnter(Collider other)
         {
+            // calling out "player" tag 
             if (other.CompareTag("Player"))
             {
-                pickup(other);
+                //function to start 
+                StartCoroutine(Pickup(other));
             }
         }
-        void pickup(Collider player)
+        IEnumerator Pickup(Collider player)
         {
-            TankHealth stats = player.GetComponent<TankHealth>();
+            //call out when player picks up Powerup
+            Debug.Log("BIG MAN!!"); 
 
-            stats.maxHealth *= multiplier;
+            //calling from playerstats script 
+            PlayerStats stats = player.GetComponent<PlayerStats>();
 
-            Destroy(gameObject); 
+            // disabling meshrenderer and collider on gameobject
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+            
+            // changing player scale when picking up powerup 
+            // waiting for 5 second before it goes back to its normal size
+            player.transform.localScale *= multiplier;
+
+            yield return new WaitForSeconds(normalSizetank);
+
+            player.transform.localScale /= multiplier;
+
+            // increases player health when powerup has been triggered
+            // and returning health back to its normal health
+            stats.health *= multiplier;
+
+            yield return new WaitForSeconds(duration);
+
+            stats.health /= multiplier;
+
+            // destroying gameobject on pickup
+            Destroy(gameObject);
         }
     }
 }
