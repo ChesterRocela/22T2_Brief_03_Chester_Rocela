9c7f13b413604be4443139dc53221c98be0444c6
ChesterRocela <107157644+ChesterRocela@users.noreply.github.com>
2022-08-11 21:26:55 +1000

    commit

--- Gad170 brief 03 Starting Point (1)/Assets/Feature.Chester/Scripts/RestartButton.cs
+++ Gad170 brief 03 Starting Point (1)/Assets/Feature.Chester/Scripts/RestartButton.cs
@@ -7,19 +7,6 @@ namespace ChesterRocela
 {
     public class RestartButton : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        // void to reset the game 
         public void ResetTheGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
